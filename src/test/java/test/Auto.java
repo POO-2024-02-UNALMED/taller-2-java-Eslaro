9170d09088f55a3d6e81cce164219a75c1c2be96
Eslaro <semoralesm@unal.edu.co>
2024-11-08 23:32:30 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -7,28 +7,31 @@ public class Auto {
 	String marca;
 	Motor motor;
 	int registro;	
-	String cantidadCreados;
+	static String cantidadCreados;
 	
-	public int cantidadAsientos(){
-	    int contador = 0;
-	    for (Asiento asiento : asientos) {
-	        if (asiento != null) {
-	            contador++;
-	        }
-	    }
-	    return contador;
-	}
-	public String verificarIntegridad() {
-	    if (motor == null || asientos == null) {
-	        return "Las piezas no son originales";
-	    }
-	    
-	    for (Asiento asiento : asientos) {
-	        if (asiento != null && (asiento.registro != registro || motor.registro != registro)) {
-	            return "Las piezas no son originales";
-	        }
-	    }
-	    
-	    return "Auto original";
-	}
+	public int cantidadAsientos() {
+        int contador = 0;
+        if (asientos != null) {  
+            for (Asiento asiento : asientos) {
+                if (asiento != null) {
+                    contador++;
+                }
+            }
+        }
+        return contador;
+    }
+
+    public String verificarIntegridad() {
+        if (motor == null || asientos == null) {
+            return "Las piezas no son originales";
+        }
+        
+        for (Asiento asiento : asientos) {
+            if (asiento != null && (asiento.registro != registro || motor.registro != registro)) {
+                return "Las piezas no son originales";
+            }
+        }
+        
+        return "Auto original";
+    }
 }
