2b3d9ea527f6a7c6caff0cccf938d4c0627ca2c8
Eslaro <semoralesm@unal.edu.co>
2024-11-08 19:45:33 -0500

    Porfavor funciona

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,5 +1,22 @@
 package test;
 
 public class Auto {
-
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	String cantidadCreados;
+	
+	public int cantidadAsientos(){
+		return asientos.length;
+	}
+	public String verificarIntegridad(){
+		if(Asiento.registro == Motor.registro) {
+			return "Auto original";
+			
+		}
+		return "Las piezas no son originales";
+	}
 }
