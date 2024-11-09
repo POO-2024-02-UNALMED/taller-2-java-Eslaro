1799863cc5f0e5fe4d1933debc047981667a2ae8
Eslaro <semoralesm@unal.edu.co>
2024-11-08 20:25:38 -0500

    Update Motor.java

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,5 +1,15 @@
 package test;
 
 public class Motor {
-
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	public void cambiarRegistro(int Nuevoregistro) {
+		this.registro = Nuevoregistro;
+	}
+	public void asignarTipo(String tipo) {
+		if(tipo == "gasolina" || tipo == "electrico") {
+			this.tipo = tipo;		
+		}
+	}
 }
